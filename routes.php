16a05c0b3e10204f5dc27456cff897ff24b38344
Rafaela <rafaelacristina.maia@gmail.com>
2026-06-22 20:19:16 -0300

    aula04

--- routes.php
+++ routes.php
@@ -2,12 +2,14 @@
 
 require_once __DIR__ . '/app/Controllers/AuthController.php';
 require_once __DIR__ . '/app/Controllers/UsuariosController.php';
+require_once __DIR__ . '/app/Controllers/PessoasController.php';
 require_once __DIR__ . '/app/Middleware/auth.php';
 
 $controller = $_GET['controller'] ?? 'auth';
 $action = $_GET['action'] ?? 'login';
 
 switch ($controller) {
+
     case 'auth':
         $authController = new AuthController();
 
@@ -15,15 +17,19 @@ switch ($controller) {
             case 'login':
                 $authController->exibirLogin();
                 break;
+
             case 'entrar':
                 $authController->entrar();
                 break;
+
             case 'dashboard':
                 $authController->dashboard();
                 break;
+
             case 'logout':
                 $authController->logout();
                 break;
+
             default:
                 http_response_code(404);
                 echo 'Acao de autenticacao nao encontrada.';
@@ -32,27 +38,67 @@ switch ($controller) {
 
     case 'usuarios':
         exigirAutenticacao();
+
         $usuariosController = new UsuariosController();
 
         switch ($action) {
+
             case 'listar':
                 $usuariosController->listar();
                 break;
+
             case 'buscarPorId':
                 $usuariosController->buscarPorId();
                 break;
+
             case 'criar':
                 $usuariosController->criar();
                 break;
+
             case 'atualizar':
                 $usuariosController->atualizar();
                 break;
+
             case 'excluir':
                 $usuariosController->excluir();
                 break;
+
             default:
                 http_response_code(404);
                 echo 'Acao de usuarios nao encontrada.';
+        }
+        break;
+
+    case 'pessoas':
+        exigirAutenticacao();
+
+        $pessoasController = new PessoasController();
+
+        switch ($action) {
+
+            case 'listar':
+                $pessoasController->listar();
+                break;
+
+            case 'buscarPorId':
+                $pessoasController->buscarPorId();
+                break;
+
+            case 'criar':
+                $pessoasController->criar();
+                break;
+
+            case 'atualizar':
+                $pessoasController->atualizar();
+                break;
+
+            case 'excluir':
+                $pessoasController->excluir();
+                break;
+
+            default:
+                http_response_code(404);
+                echo 'Acao de pessoas nao encontrada.';
         }
         break;
 
